af8e74b621dab6ebe6e36b39214b74af25cf26ee
AlekseySteblivets <aleksey.steblivets@gmail.com>
2022-04-20 18:23:42 +0300

    сделал stages для моб и планшета, плюс подправил центрирование контейнера

--- js/slider.js
+++ js/slider.js
@@ -1,4 +1,22 @@
-$('.slider').slick({});
+$('.slider').slick({
+  initialSlide: 0,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 2,
+      },
+    },
+    {
+      initialSlide: 3,
+      breakpoint: 1169,
+      settings: {
+        slidesToShow: 1.68,
+      },
+    },
+  ],
+  mobileFirst: true,
+});
 
 $('.slider-examples').slick({
   rows: 3,
