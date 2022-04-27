1eadc64073618c06c8a06a10753cf93dfdaaeaf5
AlekseySteblivets <aleksey.steblivets@gmail.com>
2022-04-27 21:00:04 +0300

    fixed accordion(js and styles )

--- js/accordion.js
+++ js/accordion.js
@@ -1,24 +1,32 @@
 (() => {
-  const menuActionServices = document.querySelectorAll('.services-item-button-svg');
+  const menuActionServices = document.querySelectorAll('.spoiler-items');
 
   console.log(menuActionServices);
 
-  menuActionServices.forEach(x => {
-    x.addEventListener('click', e => {
-      const svgTarget = e;
-      console.log(svgTarget);
-      console.log(e.target.parentElement);
-      const serviceDescrElem =
-        e.target.parentElement.parentElement.getElementsByClassName('spoiler-text')[0];
-      console.log(serviceDescrElem);
-      const shouldBeOpened = !serviceDescrElem.classList.contains('active-service-description');
+  menuActionServices.forEach(elLi => {
+    elLi.addEventListener('click', e => {
+      // console.log(elLi);
+      const isActiveClass = !elLi.children[1].classList.contains('active-service-description');
+      const elDivToAddActiveClass = e.currentTarget.getElementsByClassName('spoiler-text')[0];
+      const elSvg = e.currentTarget.getElementsByClassName('services-item-button-svg')[0];
+      // console.log(elSvg);
+      // console.log(elDivToAddActiveClass);
+      // console.log(isActiveClass);
+      // console.log(e.target);
+      if (isActiveClass) {
+        menuActionServices.forEach(el => {
+          // console.log(el.children[0].children[0]);
+          if (el.children[1].classList.contains('active-service-description')) {
+            el.children[1].classList.remove('active-service-description');
+            el.children[0].children[0].classList.remove('active-svg');
+          }
+        });
+        elDivToAddActiveClass.classList.add('active-service-description');
 
-      if (shouldBeOpened) {
-        serviceDescrElem.classList.add('active-service-description');
-        e.target.style.transform = 'rotate(180deg)';
+        elSvg.classList.add('active-svg');
       } else {
-        serviceDescrElem.classList.remove('active-service-description');
-        e.target.style.transform = 'rotate(0deg)';
+        elDivToAddActiveClass.classList.remove('active-service-description');
+        elSvg.classList.remove('active-svg');
       }
     });
   });
